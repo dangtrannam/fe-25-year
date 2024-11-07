edd49c300095a696c498bd1b0fc60dc0995c484f
NhutMinhIT <nhutminh.it19@gmail.com>
2024-11-08 00:04:42 +0700

    feat introduction page

--- src/pages/introduction.page.jsx
+++ src/pages/introduction.page.jsx
@@ -3,16 +3,20 @@ import HeaderComponent from '../components/header.component';
 
 const IntroductionPage = ({ setNextPage }) => {
     return (
-        <>
+        <div>
             <HeaderComponent />
-            <div className="flex flex-col items-center justify-center">
-                <div className="flex flex-col items-center text-center mt-4 md:mt-6">
-
+            <div className="flex flex-col items-center justify-center mt-6 px-4">
+                <div className="flex flex-col gap-4 md:gap-6 items-center text-center">
+                    <div className="flex flex-col gap-2 items-center text-center font-unbounded text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold">
+                        <span>CHÀO MỪNG ĐẾN VỚI</span>
+                        <span>HÀNH TRÌNH 25 NĂM</span>
+                    </div>
+                    <div className='w-3/4 max-w-4xl bg-white backdrop-blur-sm rounded-lg p-6 mt-4 border border-white/20 h-80'>
+                        {/* Content counter */}
+                    </div>
                 </div>
-
-
             </div>
-        </>
+        </div>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      unbounded: ['"Unbounded"', 'sans-serif'],
+    },
   },
   plugins: [],
 }
