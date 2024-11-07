a2b0b0997114e0d8fa1dbb4458731cce8d4ff756
NhutMinhIT <nhutminh.it19@gmail.com>
2024-11-07 10:09:18 +0700

    header and start page

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -2,30 +2,36 @@ import { Link } from "react-router-dom";
 
 const HeaderComponent = () => {
     return (
-        <div className="absolute top-0 left-0 right-0 md:min-h-[100px] px-4">
-            <div className="flex justify-center items-center py-3 gap-6 w-auto max-w-screen-xl mx-auto">
-                <Link
-                    to="#"
-                    target="_blank"
-                >
-                    <img
-                        src="/images/logo_fptu.png"
-                        alt="fptu-logo"
-                        className="w-24 md:w-32 lg:w-40 h-auto"
-                    />
-                </Link>
-                <Link
-                    to="3"
-                    target="_blank"
-                >
-                    <img
-                        src="/images/logo_25year.png"
-                        alt="Logo FTPU"
-                        className="w-24 h-24"
-                    />
-                </Link>
+        <header className="p-4">
+            <div className="flex items-center justify-between max-w-screen-xl mx-auto">
+                <div className="text-white font-semibold flex flex-col items-start">
+                    <span>
+                        Chọn ngôn ngữ
+                    </span>
+                    <button>
+                        Vn/Eng
+                    </button>
+                </div>
+
+                <div className="flex gap-6 items-center">
+                    <Link to="#" target="_blank">
+                        <img
+                            src="/images/logo_fptu.png"
+                            alt="FPT University Logo"
+                            className="w-20 md:w-28 lg:w-32 h-auto"
+                        />
+                    </Link>
+                    <Link to="#" target="_blank">
+                        <img
+                            src="/images/logo_25year.png"
+                            alt="25 Years Logo"
+                            className="w-20 md:w-24 lg:w-28 h-auto"
+                        />
+                    </Link>
+                </div>
+                <div className="w-24"></div>
             </div>
-        </div>
+        </header>
     );
 }
 
--- src/pages/start.page.jsx
+++ src/pages/start.page.jsx
@@ -6,19 +6,15 @@ const StartPage = ({ setNextPage }) => {
     return (
         <>
             <HeaderComponent />
-            <div
-                className="flex flex-col items-center justify-center h-screen"
-            >
-                <div className="absolute top-4 left-4">
-                    <button className="text-white font-semibold">Chọn ngôn ngữ Vn/Eng</button>
-                </div>
+            <div className="flex flex-col items-center justify-center mt-6">
 
-                <div className="flex flex-col items-center text-center mb-10">
+                {/* SVG Logo with margin adjustments for alignment */}
+                <div className="flex flex-col items-center text-center mb-10 mt-4 md:mt-6">
                     <SVGLogo />
                 </div>
 
                 <button
-                    className="text-white font-bold py-4 px-16 text-[32px] flex items-center justify-center bg-gradient-to-r from-[#5950C5] to-[#DF885B] border-[5px] border-white/[0.66] rounded-[39px]"
+                    className="text-white font-bold py-4 px-32 text-[32px] flex items-center justify-center bg-gradient-to-r from-[#5950C5] to-[#DF885B] border-[5px] border-white/[0.66] rounded-[39px]"
                     onClick={() => setNextPage()}
                 >
                     Start
