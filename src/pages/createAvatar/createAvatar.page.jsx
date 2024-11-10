7c145a097ab243467462d52d639ceaf5422afda6
Le Quoc Uy <lequocuyit@gmail.com>
2024-11-10 18:31:10 +0700

    Enhance responsiveness of avatar and card creation pages with dynamic canvas sizing and font adjustments

--- src/pages/createAvatar/createAvatar.page.jsx
+++ src/pages/createAvatar/createAvatar.page.jsx
@@ -62,6 +62,8 @@ const CreateAvatarPage = ({ setNextPage }) => {
     const renderTextOnCanvas = () => {
         if (!cardCanvas) return;
         const PIXEL_RATIO = 2;
+        const fontSizeHoursText = isMobile() ? 14 * PIXEL_RATIO : 24 * PIXEL_RATIO; // Smaller font size for mobile
+    
         if (name) {
             const existingText = cardCanvas.getObjects().filter(obj => obj.name === 'nameText');
             if (existingText.length > 0) existingText.forEach(obj => cardCanvas.remove(obj));
@@ -82,11 +84,11 @@ const CreateAvatarPage = ({ setNextPage }) => {
                 width: cardCanvas.width / 2,
                 selectable: false,
             });
-
+    
             cardCanvas.add(nameText);
             cardCanvas.bringToFront(nameText);
         }
-
+    
         if (date) {
             const existingHours = cardCanvas.getObjects().filter(obj => obj.name === 'hoursText');
             if (existingHours.length > 0) existingHours.forEach(obj => cardCanvas.remove(obj));
@@ -96,11 +98,11 @@ const CreateAvatarPage = ({ setNextPage }) => {
             const diffInMs = now - selectedDate;
             const hours = Math.floor(diffInMs / (1000 * 60 * 60));
             const fullText = `${hours} hours`;
-
+    
             const hoursText = new fabric.Textbox(fullText, {
                 left: cardCanvas.width / 2,
                 top: cardCanvas.height / 1.45,
-                fontSize: 24 * PIXEL_RATIO,
+                fontSize: fontSizeHoursText, // Responsive font size
                 fontFamily: 'Inter',
                 fill: '#ffffff',
                 textAlign: 'center',
@@ -112,7 +114,7 @@ const CreateAvatarPage = ({ setNextPage }) => {
                 width: cardCanvas.width / 2,
                 selectable: false,
             });
-
+    
             hoursText.set({
                 shadow: {
                     color: 'rgba(89, 80, 197, 0.5)',
@@ -125,23 +127,24 @@ const CreateAvatarPage = ({ setNextPage }) => {
             cardCanvas.add(hoursText);
             cardCanvas.bringToFront(hoursText);
         }
-
+    
         cardCanvas.renderAll();
     }
+    
 
     useEffect(() => {
         renderTextOnCanvas();
     }, [name, date, cardCanvas]);
 
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="p-6 flex flex-col items-center w-[584px]">
-                <div className="overflow-hidden flex items-center justify-center rounded-[23px]">
+        <div className="flex flex-col md:flex-row justify-center items-center p-4 md:p-0">
+            <div className="p-6 flex flex-col items-center w-full  md:max-w-[584px]">
+                <div className="overflow-hidden flex items-center justify-center rounded-[23px] w-full">
                     <canvas ref={avatarCanvasRef} id="avatarCanvas" />
                 </div>
-                <div className="mt-4 text-center">
+                <div className="mt-4 text-center w-full">
                     <div className="flex justify-center items-center mt-2 text-white border-b border-white w-full px-2">
-                        <label className="flex items-center w-full cursor-pointer justify-between text-[24px]">
+                        <label className="flex items-center w-full cursor-pointer justify-between text-[20px] md:text-[24px]">
                             <span className="mr-2">{text?.page4?.upload}</span>
                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                                 <path d="M5 20h14v-2H5v2zm7-18l-5 5h3v4h4v-4h3l-5-5zM5 16h14v-2H5v2z" />
@@ -158,11 +161,11 @@ const CreateAvatarPage = ({ setNextPage }) => {
                         <input
                             type="text"
                             placeholder={text?.page4?.namePlaceholder}
-                            className="bg-transparent outline-none text-white w-full placeholder-white text-[24px]"
+                            className="bg-transparent outline-none text-white w-full placeholder-white text-[20px] md:text-[24px]"
                             onChange={handleNameChange}
                         />
                     </div>
-                    <div className="flex justify-center items-center mt-2 text-white border-b border-white w-full px-2 text-[24px]">
+                    <div className="flex justify-center items-center mt-2 text-white border-b border-white w-full px-2 text-[20px] md:text-[24px]">
                         <input
                             type="date"
                             placeholder={text?.page4?.datePlaceholder}
@@ -172,22 +175,36 @@ const CreateAvatarPage = ({ setNextPage }) => {
                             onChange={handleDateChange}
                         />
                     </div>
-                    <button
-                        onClick={handleFinish}
-                        className="mt-6 px-4 py-2 w-full bg-gradient-to-r from-blue-400 to-purple-500 text-white rounded-full"
-                    >
-                        {text?.page4?.finish}
-                    </button>
+                    {
+                        !isMobile() && (
+                            <button
+                                onClick={handleFinish}
+                                className="mt-6 px-4 py-2 w-full bg-gradient-to-r from-blue-400 to-purple-500 text-white rounded-full text-[20px] md:text-[24px]"
+                            >
+                                {text?.page4?.finish}
+                            </button>
+                        )
+                    }
                 </div>
             </div>
 
-            <div className="flex flex-col items-center">
-                <div className="p-6 flex flex-col items-center w-[509px]">
-                    <div className="overflow-hidden flex items-center justify-center rounded-[23px]">
+            <div className="flex flex-col items-center w-full md:w-auto p-4 md:p-6">
+                <div className="flex flex-col items-center w-full max-w-[509px]">
+                    <div className="overflow-hidden flex items-center justify-center rounded-[23px] w-full">
                         <canvas ref={cardCanvasRef} id="cardCanvasRef" />
                     </div>
                 </div>
             </div>
+            {
+                        isMobile() && (
+                            <button
+                                onClick={handleFinish}
+                                className="mt-6 px-4 py-2 w-full bg-gradient-to-r from-blue-400 to-purple-500 text-white rounded-full text-[20px] md:text-[24px]"
+                            >
+                                {text?.page4?.finish}
+                            </button>
+                        )
+                    }
         </div>
     );
 };
