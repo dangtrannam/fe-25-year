c9eb85a52b1fc2e08811eb48c14d323cdb604d00
Le Quoc Uy <lequocuyit@gmail.com>
2024-11-10 20:43:25 +0700

    refactor button visibility for mobile and desktop in CreateAvatarPage

--- src/pages/createAvatar/createAvatar.page.jsx
+++ src/pages/createAvatar/createAvatar.page.jsx
@@ -217,16 +217,12 @@ const CreateAvatarPage = ({ setNextPage }) => {
                             <path d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11z" />
                         </svg>
                     </div>
-                    {
-                        !isMobile() && (
                             <button
                                 onClick={handleFinish}
-                                className="mt-6 px-4 py-2 w-full bg-gradient-to-r from-blue-400 to-purple-500 text-white rounded-full text-[20px] md:text-[24px]"
+                                className="hidden md:flex mt-6 px-4 py-2 w-full bg-gradient-to-r from-blue-400 to-purple-500 text-white rounded-full text-[20px] md:text-[24px]"
                             >
                                 {text?.page4?.finish}
                             </button>
-                        )
-                    }
                 </div>
             </div>
 
@@ -237,16 +233,12 @@ const CreateAvatarPage = ({ setNextPage }) => {
                     </div>
                 </div>
             </div>
-            {
-                isMobile() && (
                     <button
                         onClick={handleFinish}
-                        className="mt-6 px-4 py-2 w-full bg-gradient-to-r from-blue-400 to-purple-500 text-white rounded-full text-[20px] md:text-[24px]"
+                        className="flex md:hidden mt-6 px-4 py-2 w-full bg-gradient-to-r from-blue-400 to-purple-500 text-white rounded-full text-[20px] md:text-[24px]"
                     >
                         {text?.page4?.finish}
                     </button>
-                )
-            }
         </div>
     );
 };
