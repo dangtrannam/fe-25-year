0854400f49314b29254c0c85f944f749bb331075
NhutMinhIT <nhutminh.it19@gmail.com>
2024-11-11 01:21:04 +0700

    fix background and remove video

--- src/pages/clip/video.constant.jsx
+++ src/pages/clip/video.constant.jsx
@@ -1,3 +1,2 @@
 export const videoWeb = 'https://firebasestorage.googleapis.com/v0/b/koi-farm-shop-fd1ef.appspot.com/o/video_final.mp4?alt=media&token=60b97e3a-c300-4077-9143-47e7b3597947'
 export const videoMobile = 'https://firebasestorage.googleapis.com/v0/b/koi-farm-shop-fd1ef.appspot.com/o/video_final_mobile.mp4?alt=media&token=5685cc2d-be5a-4432-8716-a848ffb3cfbe'
-export const videoBackground = 'https://firebasestorage.googleapis.com/v0/b/koi-farm-shop-fd1ef.appspot.com/o/background.webm?alt=media&token=4025a497-8f9e-4c44-a45b-972479bbb8d3'
