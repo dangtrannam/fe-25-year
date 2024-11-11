fee6eabc6c6471d1024ae0295e014aebbb2a8450
Nam Dang <dangtrannam2001@gmail.com>
2024-11-11 17:09:59 +0700

    Enhance background video component with additional attributes for better playback control

--- src/components/background.component.jsx
+++ src/components/background.component.jsx
@@ -5,9 +5,12 @@ const BackgroundVideo = ({ src, type = "video/mp4" }) => {
         <div className="fixed inset-0 -z-10 overflow-hidden flex justify-center items-center">
             <video
                 className="absolute w-full h-full object-cover object-center"
-                autoPlay
-                loop
-                muted
+                playsInline={true}
+                muted={true}
+                autoPlay={true}
+                loop={true}
+                controls={false}
+                preload="auto"
             >
                 <source src={src} type={type} />
                 Your browser does not support the video tag.
